d3ac92291cbe5c6107ea53e97fbae36c35384f49
PopSlime <popslime@qq.com>
2023-05-07 10:54:26 +0800

    Use `lock_guard` in native C++.

--- Cryville.Input.Unity.Android.Native/jni/AndroidInputProxy.cpp
+++ Cryville.Input.Unity.Android.Native/jni/AndroidInputProxy.cpp
@@ -21,20 +21,15 @@ mutex _aip_lock;
 
 extern "C" {
 	JNIEXPORT void JNICALL Java_world_cryville_input_unity_android_NativeMethods_feed(JNIEnv* env, jobject clazz, jint hid, jint id, jint action, jlong time, jfloat x, jfloat y, jfloat z, jfloat w) {
-		_aip_lock.lock();
+		lock_guard<mutex> lock(_aip_lock);
 		_aip_frame_queue.push({ hid, id, action, time, x, y, z, w });
-		_aip_lock.unlock();
 	}
 
 	int32_t AndroidInputProxy_Poll(ProxiedInputFrame* result) {
-		_aip_lock.lock();
-		if (_aip_frame_queue.empty()) {
-			_aip_lock.unlock();
-			return 0;
-		}
+		lock_guard<mutex> lock(_aip_lock);
+		if (_aip_frame_queue.empty()) return 0;
 		*result = _aip_frame_queue.front();
 		_aip_frame_queue.pop();
-		_aip_lock.unlock();
 		return 1;
 	}
 }
